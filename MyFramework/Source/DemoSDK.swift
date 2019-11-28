37ddbdcd0ffeaab0ab6b1ef1dda5b8e27c16b524
Muhammad Jahangir Zafar <muhammad.zafar@nexusgroup.com>
2019-11-28 16:19:20 +0100

    first push

---
+++ MyFramework/Source/DemoSDK.swift
@@ -0,0 +1,16 @@
+//
+//  DemoSDK.swift
+//  MyFramework
+//
+//  Created by muhammad.zafar on 2019-11-28.
+//  Copyright © 2019 NexusGroup. All rights reserved.
+//
+
+import UIKit
+
+open class DemoSDK {
+    static let shared = DemoSDK()
+    func foo() {
+        print("Yoo man this is test")
+    }
+}
